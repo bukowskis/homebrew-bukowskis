3b35bca747cc3be0e00765128d623221c1a1f5db
Jens Norrgrann <jens.norrgrann@bukowskis.com>
2013-11-28 15:28:28 +0100

    Update leader to 1.0.5

--- leader.rb
+++ leader.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Leader < GithubGistFormula
   homepage 'https://github.com/bukowskis/leader'
-  url 'https://raw.github.com/bukowskis/leader/51c9952efcaf9c728a04fa966f5c3dbeb5d601d5/leader'
-  sha1 '751f23dcb6085f45f6b219367d50d434b1d844a1'
-  version '1.0.4'
+  url 'https://raw.github.com/bukowskis/leader/e1c6051121dc72b95330b18e21515b7bdd035ea0/leader'
+  sha1 '0dcc0fbc3442dd101a38ce06e2512ca90c7fdd98'
+  version '1.0.5'
 end
