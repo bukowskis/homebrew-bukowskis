65bd54b8f5547f58a74d448d3a67b0ff3120d251
mmzoo <mmzoo@posteo.is>
2014-12-15 11:48:54 +0100

    updated leader

--- leader.rb
+++ leader.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Leader < GithubGistFormula
   homepage 'https://github.com/bukowskis/leader'
-  url 'https://raw.github.com/bukowskis/leader/e1c6051121dc72b95330b18e21515b7bdd035ea0/leader'
-  sha1 '0dcc0fbc3442dd101a38ce06e2512ca90c7fdd98'
-  version '1.0.5'
+  url 'https://raw.github.com/bukowskis/leader/80aabb26b881c24c0e401973986a607fa1af265f/leader'
+  sha1 'd2ab2e1a2e265cdd8d657537453fca521c466106'
+  version '1.1.0'
 end
