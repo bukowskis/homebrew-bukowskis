5f09452a6ec5530745b791edb575c8f16215b7f9
mmzoo <mmzoo@posteo.is>
2014-12-15 11:50:40 +0100

    Fixed a SHA mismatch

--- leader.rb
+++ leader.rb
@@ -3,6 +3,6 @@ require 'formula'
 class Leader < GithubGistFormula
   homepage 'https://github.com/bukowskis/leader'
   url 'https://raw.github.com/bukowskis/leader/80aabb26b881c24c0e401973986a607fa1af265f/leader'
-  sha1 'd2ab2e1a2e265cdd8d657537453fca521c466106'
+  sha1 '95a7cd2b3b0de5af54a83422814df19f3d4d3fb5'
   version '1.1.0'
 end
